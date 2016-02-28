9b434603075b0da35248ddb22c9e223ee5add78b
dustinltran <dustin_tran@live.com>
2016-02-28 11:30:58 -0800

    New Class
    Added Driver Class. Someone do RideSchedule

--- src/RideSchedule.java
+++ src/RideSchedule.java
@@ -1,4 +1,10 @@
+import java.util.ArrayList;
 
 public class RideSchedule {
+	private ArrayList<String> routes;
+	private ArrayList<String> stops;
+	private ArrayList<Integer> times;
+	private ArrayList<Integer> dates;
+	private ArrayList<String> locations;
 
 }
---
+++ src/ScoopUp.java
@@ -0,0 +1,7 @@
+
+public class ScoopUp {
+
+	public static void main(String [] args){
+		
+	}
+}
