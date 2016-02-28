3e9f009ff4bc70ff341272b6164706197ecc995b
dustinltran <dustin_tran@live.com>
2016-02-27 16:31:52 -0800

    Initial Commit
    Need to flesh out the 3 classes

---
+++ src/Member.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Member {
+	private int status;
+	private String address;
+	private ArrayList<String> vehicles;
+	private boolean [] preference; //Member should have multiple statuses
+	
+	public Member()
+}
