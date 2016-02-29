572de3361561bb975d7f4b386c15ee527bb59a57
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-02-29 12:39:45 -0800

    Added driver and passenger to Scoopup

--- src/ScoopUp.java
+++ src/ScoopUp.java
@@ -3,5 +3,19 @@ public class ScoopUp {
 
 	public static void main(String [] args){
 		
+		// Adding a driver
+		Member Tim = new Member();
+		Tim.setStatus(2);
+		Tim.setAddress("1313 Like Ln San Jose CA");
+		Tim.addVehicle("2010 Audi A4");
+		Tim.setPreference(0);
+		
+		// Adding a passenger
+		Member Jack = new Member();
+		Jack.setStatus(1);
+		Jack.setAddress("636 Milky Way Ln San Jose CA");
+		Jack.setPreference(0);
+		
+		
 	}
 }
