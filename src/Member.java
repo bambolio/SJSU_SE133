3b6c3a3e8de601974d3f5ec0c13bd4e54e6ab264
Jakub Kalinowski <j.kalinowski.86@gmail.com>
2016-03-01 15:54:03 -0800

    Added username and password to member

--- src/Member.java
+++ src/Member.java
@@ -2,6 +2,36 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 public class Member {
+	private String username;
+	private String password;
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
 	private int status;
 	private String address;
 	private ArrayList<String> vehicles;
