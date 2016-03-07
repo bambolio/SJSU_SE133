83465b827d0ea2e9c100bc656e345be7199452e6
ladimer1991 <ladimer.elyeh@gmail.com>
2016-03-06 22:34:26 -0800

    a new version of menu
    this still needs work. you can register, and then sign in but it will only write a message to console saying hello (name).
    Just a better menu. also i made some random drivers with location. they are in the list everytime we run the program. so if some one can connect them to the logic using the menu that will be great.

--- src/ScoopUp.java
+++ src/ScoopUp.java
@@ -1,98 +1,176 @@
-import java.util.Scanner;
 
-public class ScoopUp {
+		
+	     
+	     
+//	     Driver driver1 = new Driver(false,-121.9092602 ,37.3451916 ); // north
+//	     Driver driver2 = new Driver(false,-121.8292602 ,37.3451916 ); // south
+//	     Driver driver3 = new Driver(false,-121.8892602 ,37.3651916 ); //east 
+//	     Driver driver4 = new Driver(false,-121.8892602 ,37.3151916 ); //west
+//	     
+//	     System.out.println( " Arraylist size is : " + Driver.drivers.size() );
+//	     System.out.println(driver1.getLocationX());
+//	     System.out.println(driver2.getLocationX());
+//	     System.out.println(driver3.getLocationX());
+//	     System.out.println(driver4.getLocationX());
+	     
 
-	public static void main(String [] args){
+		/////////////////////////////////////////
+	     import java.util.Scanner;
 
-		int option;
-		String email;
-		String password;
-		Member newMember = new Member();
-		
-		Scanner in = new Scanner(System.in);
-		
-		System.out.println("***Welcome to ScoopUp*** \nPress 1 to Sign Up \nPress 2 to Sign In \nPress 3 to Exit\n");
-		option = in.nextInt();
-		
-		if (option > 3 || option < 1) {
-			System.out.println("Invalid input. Try again");
-		}
-		
-		if (option == 1) {
-			
-			System.out.println("***Sign Up*** \nEnter your email address: ");
-			newMember.setUsername(in.next());
-			System.out.println("Enter your password: ");
-			newMember.setPassword(in.next());
-			// To Do: add forgot password
-			System.out.println("***Create User Profile*** \nEnter first name: ");
-			newMember.setFirstName(in.next());
-			System.out.println("Enter last name: ");
-			newMember.setLastName(in.next());
-			System.out.println("Enter cell number: ");
-			newMember.setCellNumber(in.next());
-			System.out.println("Enter home address: ");
-			newMember.setAddress(in.next());
-			System.out.println("Enter driver license number (driver's only) or press enter: ");
-			newMember.setLicenseNumber(in.next());
-			option = 2;
-		}
-		
-		if (option == 2) {
-			System.out.println("Enter email address: ");
-			email = in.next();
-			System.out.println("Enter password");
-			password = in.next();
-			
-			if (email.equals(newMember.getUsername()) && password == newMember.getPassword()) {
-			System.out.println("\n\n***Main Menu***\nPress 1 to Get a ride \nPress 2 to Give a ride \nPress 3 to Add a vehicle \nPress 4 to Edit you profile");
-			option = in.nextInt();
-			} else {
-				System.out.println("Invalid user's credentials. Try again");
-			}
-			if (option > 3 || option < 1) {
-				System.out.println("Invalid input. Try again");
-			}
-			
-			if (option == 1) {
-				System.out.println("***Get a ride***");
-				
-			}
-			
-			if (option == 2) {
-				System.out.println("***Give a ride***");
-				
-			}
-			
-			if (option == 3) {
-				System.out.println("***Add a vehicle***");
-				Vehicle newVehicle = new Vehicle();
-				System.out.println("Enter year");
-				newVehicle.setYear(in.nextInt());
-				System.out.println("Enter make");
-				newVehicle.setMake(in.next());
-				System.out.println("Enter model");
-				newVehicle.setModel(in.next());
-				System.out.println("Enter color");
-				newVehicle.setColor(in.next());
-				System.out.println("Enter number of available seats");
-				newVehicle.setSeatNumber(in.nextInt());
-				System.out.println("Enter license plate number");
-				newVehicle.setLicensePlate(in.next());
-			}
-			
-			if (option == 4) {
-				System.out.println("***Edit your profile***");
-				
-			}
-			if (option == 5) {
-			
-				
-			}
-		}
-		if (option == 3) {
-			
-		}
-		
-	}
-}
+	     public class ScoopUp 
+	     {
+	     	public static void main(String [] args)
+	     	{
+	     		int[] departures = new int[10];
+	     		int[] arrivals = new int[10];
+	     		
+	     		
+	     		// making some random drivers.
+	     	     Driver driver1 = new Driver(false,-121.9092602 ,37.3451916 ); // north
+	    	     Driver driver2 = new Driver(false,-121.8292602 ,37.3451916 ); // south
+	    	     Driver driver3 = new Driver(false,-121.8892602 ,37.3651916 ); //east 
+	    	     Driver driver4 = new Driver(false,-121.8892602 ,37.3151916 ); //west
+	    	     
+	    	     System.out.println( " Arraylist size is : " + Driver.drivers.size() );
+	     		
+
+	     		repeater(1);
+
+	     		// Add a Driver
+	     		Member Tim = new Member();
+	     		Tim.setStatus(2);
+	     		Tim.setAddress("1313 Like Ln San Jose CA");
+	     		//Tim.addVehicle("2010 Audi A4");
+	     		Tim.setPreference(0);
+	     		departures[0] = 800;
+	     		departures[1]  = 800;
+	     		departures[2] = 800;
+	     		departures[3]  = 800;
+	     		departures[4] = 1100;
+	     		arrivals[0] = 900;
+	     		arrivals[1] = 900;
+	     		arrivals[2] = 900;
+	     		arrivals[3] = 900;
+	     		arrivals[4] = 1200;
+
+	     		// Set Drivers location
+	     		RideSchedule TimsLocation = new RideSchedule();
+	     		TimsLocation.setLocation(TimsLocation.getLocation());
+
+	     		// Set Drivers schedule
+	     		MemberSchedule TimsSchedule = new MemberSchedule(arrivals, departures);
+	     		TimsSchedule.setDeparture(departures);
+	     		TimsSchedule.setArrival(arrivals);
+
+	     		// Add a Passenger
+	     		Member Jack = new Member();
+	     		Jack.setStatus(1);
+	     		Jack.setAddress("636 Milky Way Ln San Jose CA");
+	     		Jack.setPreference(0);
+	     		departures[0] = 830;
+	     		departures[1]  = 830;
+	     		departures[2] = 830;
+	     		departures[3]  = 830;
+	     		departures[4] = 1130;
+	     		arrivals[0] = 900;
+	     		arrivals[1] = 900;
+	     		arrivals[2] = 900;
+	     		arrivals[3] = 900;
+	     		arrivals[4] = 1200;
+
+	     		// Set Passengers location
+	     		RideSchedule JacksLocation = new RideSchedule();
+	     		JacksLocation.setLocation(JacksLocation.getLocation());
+
+	     		// Set Passengers schedule
+	     		MemberSchedule JacksSchedule = new MemberSchedule(arrivals, departures);
+	     		JacksSchedule.setDeparture(departures);
+	     		JacksSchedule.setArrival(arrivals);
+
+	     		// Book a ride
+	     	}
+	     	public static void  repeater(int i )
+	     	{
+
+	     		if (i == 1 )
+	     		{
+	     			System.out.println(".:Main Menu:.");
+	     			System.out.println("1. Sign up");
+	     			System.out.println("2. Log in");
+	     			System.out.println("3. Exit");
+
+	     			Scanner reader = new Scanner(System.in);
+
+	     			Member member = new Member();
+
+	     			int input = Integer.parseInt(reader.nextLine());
+
+	     			if (input == 1)
+	     			{
+	     				System.out.println("Please enter your name: ");
+	     				member.setFirstName(reader.nextLine());
+
+	     				System.out.println("Please enter your email: ");
+	     				member.setEmail(reader.nextLine());
+
+	     				System.out.println("Please enter your password: ");
+	     				member.setPassword(reader.nextLine());
+
+	     				System.out.println("Please enter your address: ");
+	     				member.setAddress(reader.nextLine());
+
+
+
+	     				Vehicle vehicle = new Vehicle();
+
+	     				System.out.println("Enter your vehicle information: ");
+	     				System.out.println("Make: ");
+	     				vehicle.setMake(reader.nextLine());
+
+	     				System.out.println("Model: ");
+	     				vehicle.setModel(reader.nextLine());
+
+	     				System.out.println("Year: ");
+	     				vehicle.setYear(reader.nextInt());
+
+	     				System.out.println("Color: ");
+	     				vehicle.setColor(reader.nextLine());
+
+	     				reader.nextLine();
+	     				System.out.println("License Plate: ");
+	     				String s = reader.nextLine();
+	     				vehicle.setLicensePlate(s);
+
+	     				System.out.println("Sign up Successfull!");
+
+	     				System.out.println("1. Return to Main Menu");
+	     				System.out.println("2. Exit");
+	     				int j = reader.nextInt();
+
+	     				if (j  == 2)
+	     				{
+	     					repeater(2);
+	     				}
+	     				else if (j == 1)
+	     				{
+	     					repeater(1); 
+	     				}
+	     			}
+
+	     			else if (input == 2)
+	     			{
+	     				System.out.println("Please enter your name: ");
+	     				String name = reader.nextLine();
+
+	     				System.out.println("Please enter your password: ");
+	     				String pass = reader.nextLine();
+	     				System.out.println("Welcome, "+ name + "!");
+	     			}
+	     			else if (input == 3)
+	     			{
+	     				System.out.println("End of program!");
+	     			}
+	     		} 
+	     		System.out.println("End of program! ");
+	     	}
+	     }
