9212b0793dbf78002e1023af30c01249a67308c3
ladimer1991 <ladimer.elyeh@gmail.com>
2016-03-06 23:50:28 -0800

    Update RideSchedule.java

--- src/RideSchedule.java
+++ src/RideSchedule.java
@@ -1,67 +1,150 @@
+
 import java.util.ArrayList;
 
-public class RideSchedule {
+
+
+
+public class RideSchedule 
+
+{
+
 	private ArrayList<String> routes;
+
 	private ArrayList<Stops> stops;
+
 	private String location;
 
-	/**
-	 * Empty Constructor
-	 */
-	public RideSchedule(){
-		
-	}
-	
-	/**
-	 * Set new location
-	 * @param location location of pickup
-	 */
-	public void setLocation(String location){
-		this.location = location;
-	}
-	
-	/**
-	 * Get location
-	 * @return location
-	 */
-	public String getLocation(){
-		return location;
-	}
-	
-	/**
-	 * Add new routes
-	 * @param route route to be added
-	 */
-	public void addRoutes(String route){
-		this.routes.add(route);
-	}
+
+
 
 	/**
-	 * Add new date to schedule
-	 * @param date date of pickup
-	 * @param stop stop location
-	 * @param time time of pickup
+
+	 * Empty Constructor
+
 	 */
-	public void addDay(Integer date, String stop, Integer time){
-		Stops temp = new Stops(date);
-		temp.addStop(stop, time);
-		stops.add(temp);
+
+	public RideSchedule()
+
+	{
+
+
+
+
 	}
-	
+
+
+
+
 	/**
-	 * Add new stop to schedule
-	 * @param date date of pickup
-	 * @param stop stop location
-	 * @param time time of pickup
+
+	 * Set new location
+
+	 * @param location location of pickup
+
 	 */
-	public void addNewStop(Integer date, String stop, Integer time){
-		for(Stops temp: stops){
-			if(temp.getDate() == date){
-				temp.addStop(stop, time);
-			}
-		}
+
+	public void setLocation(String location)
+
+	{
+
+		this.location = location;
+
 	}
-	
-	
+
+
+
+
+	/**
+
+	 * Get location
+
+	 * @return location
+
+	 */
+
+	public String getLocation()
+
+	{
+
+		return location;
+
+	}
+
+
+
+
+	/**
+
+	 * Add new routes
+
+	 * @param route route to be added
+
+	 */
+
+	public void addRoutes(String route)
+
+	{
+
+		this.routes.add(route);
+
+	}
+
+
+
+
+	/**
+
+	 * Add new date to schedule
+
+	 * @param date date of pickup
+
+	 * @param stop stop location
+
+	 * @param time time of pickup
+
+	 */
+
+	public void addDay(Integer date, String stop, Integer time)
+
+	{
+
+		Stops temp = new Stops(date);
+
+		temp.addStop(stop, time);
+
+		stops.add(temp);
+
+	}
+
+
+
+
+	/**
+
+	 * Add new stop to schedule
+
+	 * @param date date of pickup
+
+	 * @param stop stop location
+
+	 * @param time time of pickup
+
+	 */
+
+	public void addNewStop(Integer date, String stop, Integer time)
+
+	{
+
+		for(Stops temp: stops){
+
+			if(temp.getDate() == date){
+
+				temp.addStop(stop, time);
+
+			}
+
+		}
+
+	}
 
 }
