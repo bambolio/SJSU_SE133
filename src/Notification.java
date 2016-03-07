f56fad72aead681a064d14221fc8effa4de39e96
ladimer1991 <ladimer.elyeh@gmail.com>
2016-03-06 23:42:50 -0800

    Update Notification.java
    demo works

--- src/Notification.java
+++ src/Notification.java
@@ -5,14 +5,15 @@ public class Notification {
 		
 		String location = Double.toString(x)+" , " + Double.toString(y) ; // this should be converted to street address later on. 
 		
-		return " A passenger is on your way and has been assigned to you "+ "\n"+
-	   " the location of passenger is : "+ location ;
+		return "Notification to Driver: " +"\n" +" A passenger is on your way and has been assigned to you "+ "\n"+
+	   " The location of passenger is : "+ location +"\n";
 	}
 
 	
 	public String sendPassengerNotification(Driver dr ) {
 		
-		String message = dr.getName() + " will pick you up. " ;; 
+		String message = "Notification to Passenger: " +"\n" + " Your request has been processed  "+ "\n" +
+		dr.getName() + " will pick you up. " ;
 	
 		//return  name + " will pick you up. " ;
 		return message;
