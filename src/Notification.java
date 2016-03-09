61f4f0b22fae8e0fba8c4fe3d3fd5257a8ce1980
ladimer1991 <ladimer.elyeh@gmail.com>
2016-03-09 08:39:08 -0800

    Update Notification.java

--- src/Notification.java
+++ src/Notification.java
@@ -5,7 +5,7 @@ public class Notification {
 		
 		String location = Double.toString(x)+" , " + Double.toString(y) ; // this should be converted to street address later on. 
 		
-		return "Notification to Driver: " +"\n" +" A passenger is on your way and has been assigned to you "+ "\n"+
+		return "Notification to Driver: " +"\n" +" A passenger has been assigned to you "+ "\n"+
 	   " The location of passenger is : "+ location +"\n";
 	}
 
